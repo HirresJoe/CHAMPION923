30b8a12e15399a264c610f4bdc7084cbf7e0bd26
CHAMPION923 <1056156912@qq.com>
2025-05-12 16:28:40 +0800

    Initial commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("n", "Q", ":bd<CR>")
+vim.keymap.set("n", ";", ":") --命令模式
+vim.keymap.set("n", "S", ":w<CR>")
